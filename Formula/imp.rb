003e515fc519bd73d25c1fc0fcbaabb124c7281b
samseen <dmsameeram@gmail.com>
2026-01-02 00:34:48 +0530

    Fix SHA256 hashes for PyPI dependencies

--- Formula/imp.rb
+++ Formula/imp.rb
@@ -11,12 +11,12 @@ class Imp < Formula
 
   resource "pillow" do
     url "https://files.pythonhosted.org/packages/f8/3e/32cbd0129a28686621434cbf17bb64bf1458bfb838f1f668262f120d3a3f/pillow-11.1.0.tar.gz"
-    sha256 "368da70808b36d73b4b390a8ffac11069f8a5c85f29eff1f1b01bcf3ef5b2a20"
+    sha256 "fd0e967d74a362f8f94fd1435aa698bc7f24cdd248a02fbc33d9e856c5c73a37"
   end
 
   resource "rich" do
     url "https://files.pythonhosted.org/packages/ab/3a/0316b28d0761c6734d6bc14e770d85506c986c85ffb239e688eeaab2c2bc/rich-13.9.4.tar.gz"
-    sha256 "439594978a49a09530cff7ebc4b5c7103ef57c16803ce7ea7e4e34e7a27eb117"
+    sha256 "439594978a49a09530cff7ebc4b5c7103ef57baf48d5ea3184f21d9a2befa098"
   end
 
   resource "markdown-it-py" do
@@ -26,12 +26,12 @@ class Imp < Formula
 
   resource "mdurl" do
     url "https://files.pythonhosted.org/packages/d6/54/cfe61301667036ec958cb99bd3ead5f90e64e89dab15ae08aac77f6c4f18/mdurl-0.1.2.tar.gz"
-    sha256 "bb413d29f5eea38f31dd4754dd7377d4465116fb207585f97bf925588687c1ba"
+    sha256 "e0173d29bcc19f0fcd3e74b44d550ca9a59126d456c5952b19794596667ad6e9"
   end
 
   resource "pygments" do
     url "https://files.pythonhosted.org/packages/7c/2d/c3338d48ea6cc0feb8446d8e6937e1408088a72a39937982c7b0ec9c9401/pygments-2.19.1.tar.gz"
-    sha256 "61c16d2a8576dc0649d9f39e089b5f02bcd27fba10d8fb4dcc28173f7a45151f"
+    sha256 "47aac1a2c2fb601510dd0e559f64f7a75e25f6ceca51ae60edef13df89a27395"
   end
 
   def install
