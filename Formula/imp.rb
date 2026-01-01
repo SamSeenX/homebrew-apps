01feb280e5ecc87b2e6c6e77192e20dc3e2907a7
samseen <dmsameeram@gmail.com>
2026-01-02 00:37:50 +0530

    Add jpeg and image library dependencies for Pillow

--- Formula/imp.rb
+++ Formula/imp.rb
@@ -8,6 +8,11 @@ class Imp < Formula
   license "MIT"
 
   depends_on "python@3.11"
+  depends_on "jpeg"
+  depends_on "libtiff"
+  depends_on "little-cms2"
+  depends_on "webp"
+  depends_on "zlib"
 
   resource "pillow" do
     url "https://files.pythonhosted.org/packages/f3/af/c097e544e7bd278333db77933e535098c259609c4eb3b85381109602fb5b/pillow-11.1.0.tar.gz"
